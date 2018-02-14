54aa168c8e6ac38ad62a8f4895d21424aadedf11
Jerry W <jerrywiltse@gmail.com>
2018-02-14 12:35:52 -0500

    refresh all

--- conanfile.py
+++ conanfile.py
@@ -1,38 +1,34 @@
 from conans import CMake, ConanFile, tools
 import os
 
-
 class OpenALConan(ConanFile):
     name = "openal"
     version = "1.18.2"
+    md5 = "fa2cb3df766ab5976c86efbcc1d24d68"
+    description = "OpenAL Soft is a software implementation of the OpenAL 3D audio API."
+    url = "http://github.com/bincrafters/conan-openal"
+    license = "MIT"
+    exports = ["LICENSE.md"]
+    exports_sources = ["CMakeLists.txt"]
     generators = "cmake"
+    source_subfolder = "source_subfolder"
     settings = "os", "arch", "compiler", "build_type"
     options = {"shared": [True, False]}
     default_options = "shared=True"
-    url = "http://github.com/bincrafters/conan-openal"
-    license = "MIT"
-    description = "OpenAL Soft is a software implementation of the OpenAL 3D audio API."
 
     def requirements(self):
         if self.settings.os == "Linux":
             self.requires("libalsa/1.1.5@conan/stable")
 
-    @property
-    def folder_name(self):
-        return "openal-soft-openal-soft-%s" % self.version
-
     def source(self):
-        zip_name = "%s-soft-%s.tar.gz" % (self.name, self.version)
-        tools.get("https://github.com/kcat/openal-soft/archive/%s" % zip_name, md5="fa2cb3df766ab5976c86efbcc1d24d68")
-        tools.replace_in_file(os.path.join(self.folder_name, "CMakeLists.txt"),
-                              "PROJECT(OpenAL)",
-                              '''PROJECT(OpenAL)
-include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
-conan_basic_setup()''')
-
+        source_url = "https://github.com/kcat/openal-soft"
+        tools.get("{0}/archive/openal-soft-{1}.tar.gz".format(source_url, self.version), self.md5)
+        extracted_dir = "openal-soft-openal-soft-" + self.version
+        os.rename(extracted_dir, self.source_subfolder)
+        
     def build(self):
         cmake = CMake(self)
-        cmake.configure(source_folder=self.folder_name)
+        cmake.configure()
         cmake.build()
         cmake.install()
 
