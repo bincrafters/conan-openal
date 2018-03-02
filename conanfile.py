d2a3f57178f67a665eb55a9db7199a615df0ddcb
SSE4 <tomskside@gmail.com>
2018-03-02 10:16:53 +0700

    - fix linking issues on OSX and Linux

--- conanfile.py
+++ conanfile.py
@@ -46,6 +46,13 @@ class OpenALConan(ConanFile):
             self.cpp_info.libs = ["OpenAL32", 'winmm']
         else:
             self.cpp_info.libs = ["openal"]
+        if self.settings.os == 'Linux':
+            self.cpp_info.libs.append('m')
+        elif self.settings.os == 'Macos':
+            frameworks = ['AudioToolbox']
+            for framework in frameworks:
+                self.cpp_info.exelinkflags.append("-framework %s" % framework)
+            self.cpp_info.sharedlinkflags = self.cpp_info.exelinkflags
         self.cpp_info.includedirs = ["include", "include/AL"]
         if not self.options.shared:
             self.cpp_info.defines.append('AL_LIBTYPE_STATIC')
